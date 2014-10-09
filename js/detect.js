4e4c9ce851fe001f08d32b500dbd2801e9fe85e8
gexplorer <gexplorer8@gmail.com>
2014-10-09 15:29:22 +0200

    Automatic tab switching and card highlighting

--- js/detect.js
+++ js/detect.js
@@ -74,7 +74,7 @@ Case.prototype.render = function(){
 
 Case.prototype.createCard = function(title, list){
 	var cardElem = document.createElement("div");
-	cardElem.id = title;
+	cardElem.id = title.substr(1);
 	cardElem.className = "panel panel-default";
 
 	var headingElem = document.createElement("div");
@@ -111,9 +111,28 @@ var Case = new Case();
 
 function Clue(location, message){
 	this.message = message;
-	this.messageHTML = message.replace(/([@#]\w+)/g, "<a href=\"#$1\">$1</a>");
-	this.badgeHTML = "<span class=\"badge\"><a href=\"#"+location+"\">"+location+"</a></span>";
+	this.messageHTML = message.replace(/([@#])(\w+)/g, "<a href=\"#$2\" onclick=\"clickClue('$1', '$2')\">$1$2</a>");
+	this.badgeHTML = "<span class=\"badge\"><a href=\"#"+location.substr(1)+"\" onclick=\"clickClue(':', '"+location.substr(1)+"')\">"+location+"</a></span>";
 	this.people = message.match(/@\w+/g);
 	this.tags = message.match(/#\w+/g);
 	this.location = location;
+}
+
+function clickClue(type, id){
+	var tab = "";
+	switch(type){
+		case ":": tab = "history";
+				break;
+		case "@": tab = "people";
+				break;
+		case "#": tab = "tags";
+				break;
+		default: tab = "editor";
+				break;
+
+
+	}
+	$('#main-menu a[href="#'+tab+'"]').tab("show")
+	$(".panel").removeClass("panel-primary")
+	$("#"+id).addClass("panel-primary");
 }
